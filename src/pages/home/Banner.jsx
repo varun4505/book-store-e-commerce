a18eaed26d8870c6e5d7ce2d269ea8cc02290c00
varun4505 <145825700+varun4505@users.noreply.github.com>
2025-01-17 15:22:00 +0000

    p

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
   return (
     <div className='flex flex-col md:flex-row-reverse py-16 justify-between items-center gap-12'>
          <div className='md:w-1/2 w-full flex items-center md:justify-end'>
-            <img src={bannerImg} alt="" />
+         <img src={bannerImg} alt="" className='w-full h-auto md:w-3/4' />
         </div>
         
         <div className='md:w-1/2 w-full'>
