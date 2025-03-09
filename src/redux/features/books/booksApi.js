6c2db84045ad073fcf227de44d8f8344a7e84785
varun4505 <145825700+varun4505@users.noreply.github.com>
2025-03-10 00:36:55 +0530

    Update booksApi.js

--- src/redux/features/books/booksApi.js
+++ src/redux/features/books/booksApi.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import getBaseUrl from '../../../utils/baseURL';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: '/api', // Use relative path to work with Vite proxy
+    baseUrl: `${getBaseUrl()}/api`, // Use the correct backend URL
     credentials: 'omit', // Don't send credentials for cross-origin requests
     prepareHeaders: (headers) => {
         headers.set('Accept', 'application/json');
