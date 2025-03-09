292dd1a52c3899554311bea0c5e1f47ef7bf96e6
varun4505 <145825700+varun4505@users.noreply.github.com>
2025-03-10 00:34:50 +0530

    Update baseURL.js

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
     // Using relative URL to leverage Vite's proxy configuration
-    return "";
+    return "https://book-app-backend-tawny.vercel.app";
 }
 
 export default getBaseUrl;
