d8ccf907d09cc67095624be0bf203e1905b48786
varun4505 <145825700+varun4505@users.noreply.github.com>
2025-03-23 16:34:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
       <AuthProvide>
         <Navbar />
     
-        <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
+        <main className='min-h-screen max-w-screen-xl mx-auto px-4 py-6 font-primary'>
           <Outlet />
         </main>
         <Footer />
