14f47e2d3a1ecf9f1fc29942ec1555477c88e1b7
varun4505 <145825700+varun4505@users.noreply.github.com>
2025-03-23 14:15:15 +0530

    Update baseURL.js

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000,https://book-app-backend-tawny.vercel.app/"
+    return "https://book-app-backend-tawny.vercel.app/"
 }
 
 export default getBaseUrl;
